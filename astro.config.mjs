6c4670f85389e67de64112209ada385572b74d7c
michaelalavelle <87865217+michaelalavelle@users.noreply.github.com>
2026-02-12 19:06:33 -0500

    Set Astro base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://michaelalavelle.github.io',
+  base: '/stagnesofassisi',
+});
